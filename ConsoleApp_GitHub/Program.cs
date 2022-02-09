ee852b8253e4ff03c7f8ab56d9842088f4fc6c4b
Emil Asadov <Emil.Asadov@rabitabank.com>
2022-02-09 21:30:33 +0400

    asdasdasd

--- ConsoleApp_GitHub/Program.cs
+++ ConsoleApp_GitHub/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp_GitHub
     {
         static void Main(string[] args)
         {
-            Console.Write(10+5);
+            Console.Write(10+5); //sdfsdfsfsdf
             Console.ReadKey();
         }
     }
