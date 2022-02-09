3cde12c949691e254c459899f1b0e6f4ac03df0f
Emil Asadov <Emil.Asadov@rabitabank.com>
2022-02-09 20:26:45 +0400

    Add console

--- ConsoleApp_GitHub/Program.cs
+++ ConsoleApp_GitHub/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp_GitHub
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello World");
             Console.Write(10+5);
             Console.ReadKey();
         }
